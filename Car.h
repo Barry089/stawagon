eff9526b1ac7203df768edd1e1f0fac10fde8fad
barry <barryss@qq.com>
2021-10-14 15:48:55 +0800

    add car.h

---
+++ Car.h
@@ -0,0 +1,27 @@
+
+
+#include <iostream>
+using namespace std;
+class Car {
+    private:
+        int power;
+        int seat;
+    public:
+        Car(int power, int seat);
+        ~Car();
+        void show();
+};
+
+Car::Car(int power, int seat)
+{
+    this->power = power;
+    this->seat = seat;
+}
+Car::~Car()
+{
+    cout << "car distructor" << endl;
+}
+void Car::show()
+{
+    cout << "car power: " << power << ", seat: " << seat << endl;
+}
